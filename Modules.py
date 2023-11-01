ac7c4bf3daeef356db8a1e91e67b7976ccecb03a
ApurvaDate <102308153+ApurvaDate@users.noreply.github.com>
2023-11-01 10:52:11 +0530

    Add files via upload

---
+++ Modules.py
@@ -0,0 +1,18 @@
+#each .py file is a module, we can build classes, function
+def multiply( num1, num2):
+    return num1*num2
+
+def divide(num1,num2):
+    return num1/num2
+
+#this is our module where it has multiply and divide functions
+#we can import these functions as 
+#from modules import multiply
+
+#by this wqe generated __pycache__ folder
+#pycache is generated when we run a file with import statements
+
+
+
+##### packages in python #####
+
